def4bd705ef4c54ec7a5dd697905edca1a38bd4a
AlejandroMolinar <101030577+AlejandroMolinar@users.noreply.github.com>
2022-04-11 13:39:41 -0400

    FormOk

--- application/views/login.php
+++ application/views/login.php
@@ -20,7 +20,7 @@
       <div class="login-html">
         <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
         <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
-        <div class="login-form">
+        <form class="login-form" action="" method="post">
           <div class="sign-in-htm">
             <div class="group">
               <label for="user" class="label">Username</label>
