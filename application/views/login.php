807bb6367c3dacba3f188298bbfe1430a270e894
AlejandroMolinar <101030577+AlejandroMolinar@users.noreply.github.com>
2022-04-12 13:51:41 -0400

    init

--- application/views/login.php
+++ application/views/login.php
@@ -27,10 +27,10 @@
     <script type="text/javascript" src="../SystemInventory/public/materialize/js/materialize.min.js"></script>
     
     <div class="login-wrap">
-      <div class="login-html" style="height: 160%;">
+      <div class="login-html">
         <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
         <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
-        <form class="login-form" action="serie.php" method="post">
+        <form class="login-form" action="logAdd" method="post">
           <div class="sign-in-htm">
             <div class="group">
               <label for="lab_ema" class="label">Correo</label>
