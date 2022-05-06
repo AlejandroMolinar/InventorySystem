806f431dca3d3f614b170b947ea3d60751156287
AlejandroMolinar <101030577+AlejandroMolinar@users.noreply.github.com>
2022-05-06 14:03:30 -0400

    Tabla Inventario Funcionando

--- application/controllers/control_inventario/ControlInventario.php
+++ application/controllers/control_inventario/ControlInventario.php
@@ -6,24 +6,31 @@ class ControlInventario extends CI_Controller
 	public function __construct(){
 		parent::__construct();
 		$this->load->database();
+		$this->load->model('Control_Inventario/Inventario_model');
+
 
 	}
 	public function index(){
 		if($this->session->userdata('is_logged')){
 			$range = $this->session->range;
-			$this->GetView($range);
+
+			$data= $this->Inventario_model->GetTable();
 			
+			if($data != null){
+				$this->GetView($range, $data);
+			}
+
 		}else{
 			redirect(base_url());
 		}
 	}
 	
-    public function GetView($range){
+    public function GetView($range, $dataTable){
 		
 		if ($range != 1) {
-			$view = $this->load->view('user/inventario_user','', TRUE);
+			$view = $this->load->view('user/inventario_user', array('table' => $dataTable) , TRUE);
 		}else{
-			$view = $this->load->view('admin/inventario_admin','', TRUE);
+			$view = $this->load->view('admin/inventario_admin', array('table' => $dataTable) , TRUE);
 		}
 		$data= array(
 			'head' => $this->load->view('layout/head','', TRUE),
@@ -32,6 +39,8 @@ class ControlInventario extends CI_Controller
 			'side' => $this->load->view('layout/sidebar','', TRUE),
 			'footer' => $this->load->view('layout/footer','', TRUE),
 		); 
+
+
 	
 		$this->load->view('inventario', $data);
 		
---
+++ application/models/Control_Inventario/Inventario_model.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Inventario_model extends CI_model{
+
+    public function __construct(){
+        $this->load->database();
+    }
+
+    public function GetTable(){
+        
+        $data_log= $this->db->get('inventario');
+        return $data_log->result();
+
+    }
+
+    public function GetTables($table, $key, $search){
+        
+        $data_log= $this->db->get_where($table, array($key => $search), 1);
+        if(!$data_log->result()){
+            return FALSE;
+        }
+        return $data_log->row();
+
+    }
+
+    public function create($data_sp){
+        if(!$this->db->insert('inventario', $data_sp)){
+            return FALSE;
+        }
+        return TRUE;
+    }
+}
