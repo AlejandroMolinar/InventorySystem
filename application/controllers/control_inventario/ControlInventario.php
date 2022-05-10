895840c49940b09114d99e62b668f0db08d51e53
AlejandroMolinar <101030577+AlejandroMolinar@users.noreply.github.com>
2022-05-10 12:39:28 -0400

    guadado

--- application/controllers/control_inventario/ControlInventario.php
+++ application/controllers/control_inventario/ControlInventario.php
@@ -19,48 +19,52 @@ class ControlInventario extends CI_Controller
 
 			foreach ($data as $items) {
 
-				$id= $items->id_inv_bien; 
-				$marca= $this->GetElements('marca', 'cod_marc', $items->cod_marc);
-				$modelo= $this->GetElements('modelo', 'id_mod', $items->id_mod_bien);
-				$serial= $items->serial_bien; 
-				$numBien=$this->GetElements('bien_mue', 'id_bien_mue', $items->id_num_bien);
-				$color= $this->GetElements('colores', 'id_col', $items->id_clr_bien);
-				$componente= $this->GetElements('tp_comp', 'id_tp_comp', $items->id_tpc_bien);
-				$undAdm= $this->GetElements('uni_adm', 'id_uni_adm', $items->id_adm_bien);
-				$trabajador= $this->GetElements('trabajador', 'id_trb', $items->id_trb_bien);
-				$ciudad= $this->GetElements('ciudad', 'id_ciu', $items->id_ciu_bien);
-				$municipio= $this->GetElements('municipio', 'id_mun', $items->id_mun_bien);
-				$parroquia= $this->GetElements('parroquia', 'id_parr', $items->id_parr_bien);
-				$fecha= $items->fec_crt_inv; 
-				$hora= $items->hor_crt_inv;
-				$status= $items->status;
-
-				$datos= array(
-					'id' => $id,
-					'marca' => $marca -> den_com_marc,
-					'modelo' => $modelo -> den_mod,
-					'serial' => $serial,
-					'numBien' => $numBien -> num_bien_mue,
-					'color' => $color -> desc_col,
-					'componente' => $componente -> mat_tp_comp,
-					'undAdm' => $undAdm -> desc_uni_adm,
-					'nombre' => $trabajador -> nombre_trb,
-					'apellido' => $trabajador -> apellido_trb,
-					'ciudad' => $ciudad -> desc_ciu,
-					'municipio' => $municipio -> desc_mun,
-					'parroquia' => $parroquia -> desc_parr,
-					'fecha' => $fecha,
-					'hora' => $hora,
-					'status' => $status,
+				$datos = array(
+					'id' => $items->id_inv_bien, 
+					'marca' => $this->GetElements('marca', 'cod_marc', $items->cod_marc),
+					'modelo' => $this->GetElements('modelo', 'id_mod', $items->id_mod_bien),
+					'serial' => $items->serial_bien,
+					'numBien' => $this->GetElements('bien_mue', 'id_bien_mue', $items->id_num_bien),
+					'color' => $this->GetElements('colores', 'id_col', $items->id_clr_bien),
+					'componente' => $this->GetElements('tp_comp', 'id_tp_comp', $items->id_tpc_bien),
+					'undAdm' => $this->GetElements('uni_adm', 'id_uni_adm', $items->id_adm_bien),
+					'trabajador' => $this->GetElements('trabajador', 'id_trb', $items->id_trb_bien),
+					'ciudad' => $this->GetElements('ciudad', 'id_ciu', $items->id_ciu_bien),
+					'municipio' => $this->GetElements('municipio', 'id_mun', $items->id_mun_bien),
+					'parroquia' => $this->GetElements('parroquia', 'id_parr', $items->id_parr_bien),
+					'fecha' =>  $items->fec_crt_inv,
+					'hora' => $items->hor_crt_inv,
+					'status' => $items->status,
 				);
+				
+				// foreach ($datos as $key) {
+				// 	$datosLimpios= array(
+				// 		'id' => $id,
+				// 		'marca' => $marca -> den_com_marc,
+				// 		'modelo' => $modelo -> den_mod,
+				// 		'serial' => $serial,
+				// 		'numBien' => $numBien -> num_bien_mue,
+				// 		'color' => $color -> desc_col,
+				// 		'componente' => $componente -> mat_tp_comp,
+				// 		'undAdm' => $undAdm -> desc_uni_adm,
+				// 		'nombre' => $trabajador -> nombre_trb,
+				// 		'apellido' => $trabajador -> apellido_trb,
+				// 		'ciudad' => $ciudad -> desc_ciu,
+				// 		'municipio' => $municipio -> desc_mun,
+				// 		'parroquia' => $parroquia -> desc_parr,
+				// 		'fecha' => $fecha,
+				// 		'hora' => $hora,
+				// 		'status' => $status,
+				// 	);
+				// }
 				array_push($datatotal, $datos);
 			}
 
-			// foreach ($datatotal as $key) {
-				// foreach ($key as $item) {
-					var_dump($datos);
-				// }
-			// }
+			foreach ($datatotal as $key) {
+				foreach ($key['marca'] as $item) {
+					var_dump($item->den_com_marc);
+				}
+			}
 
 			if ($data != null) {
 				$this->GetView($range, $datatotal);
